5ce6b164351fcdaf96c277dbfd5b7b5f2677cca0
Arevik-Sunny <arevik.abrahamyan18@gmail.com>
2023-06-09 14:42:08 -0400

    Add seeds values

---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+INSERT INTO departments
+(department_name)
+VALUES
+('Finace'),
+('HR'),
+('Marketing'),
+('Customer Service'),
+('IT'),
+('Legal'),
+
+INSERT INTO roles(title,salary,department_id)
+VALUES
+('Finance', 130000.00,1),
+('HR',150000.00,2),
+('Marketing',100000.00,3),
+('Customer Service', 50000.00,4),
+('IT', 250000.00,5),
+('Legal',350000.00,6);
+
+INSERT INTO employee (first_name,last_name,role_id,)
+VALUES
+('Anush','Badalyan',1),
+('Vazgen', 'Karapetyan',2),
+('Jordan','Pedersen', 3),
+('Laura' 'Melikyan', 4),
+('Mariam Simityan', 5),
+('Lina Amiryan',6),
